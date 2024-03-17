8552f00949e0de322ec9dc85439ca32d11f67d8b
02_bao <104081842+02bao@users.noreply.github.com>
2024-03-17 21:26:05 +0700

    update

---
+++ FoodApi/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace FoodApi.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Detail { get; set; }
+        public string ImageUrl { get; set; }
+        public double Price { get; set; }
+        public bool IsPopularProduct { get; set; }
+        public int CategoryId { get; set; }
+        [NotMapped]
+        public byte[] ImageArray { get; set; }
+        [JsonIgnore]
+        public ICollection<OrderDetail> OrderDetails { get; set;}
+        [JsonIgnore]
+        public ICollection<ShoppingCartItem> ShoppingCartItems { get; set; }
+    }
+}
---
+++ FoodApi/Models/ShoppingCartItem.cs
@@ -0,0 +1,12 @@
+﻿namespace FoodApi.Models
+{
+    public class ShoppingCartItem
+    {
+        public int Id { get; set; }
+        public int Qty { get; set; }
+        public double Price { get; set; }
+        public double TotalAmount { get; set; }
+        public int ProductId { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
