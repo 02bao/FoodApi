f6f679727765d863ec41b4ee4be5d1c57fb91a82
02_bao <104081842+02bao@users.noreply.github.com>
2024-03-18 12:16:25 +0700

    Update Accounts Api

--- FoodApi/Controllers/AccountsController.cs
+++ FoodApi/Controllers/AccountsController.cs
@@ -1,6 +1,10 @@
 ﻿using AuthenticationPlugin;
 using FoodApi.Data;
+using FoodApi.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 
 namespace FoodApi.Controllers
@@ -9,8 +13,8 @@ namespace FoodApi.Controllers
     [Route("api/[controller]/[action]")]
     public class AccountsController : ControllerBase
     {
-        private  FoodDbContext _dbContext;
-        private  IConfiguration _configuration;
+        private FoodDbContext _dbContext;
+        private IConfiguration _configuration;
         private readonly AuthService _auth;
 
         public AccountsController(FoodDbContext dbContext, IConfiguration configuration)
@@ -19,5 +23,63 @@ namespace FoodApi.Controllers
             _configuration = configuration;
             _auth = new AuthService(_configuration);
         }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(User user)
+        {
+            var UserWithSameEmail = _dbContext.Users.SingleOrDefault(u => u.Email == user.Email);
+            if (UserWithSameEmail != null)
+            {
+                return BadRequest("User with this email already exists");
+            }
+            var userObj = new User
+            {
+                Name = user.Name,
+                Email = user.Email,
+                Password = SecurePasswordHasherHelper.Hash(user.Password),
+                Role = "User"
+            };
+            _dbContext.Users.Add(userObj);
+            await _dbContext.SaveChangesAsync();
+            return StatusCode(StatusCodes.Status201Created);
+
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Login(User user)
+        {
+            var userEmail = _dbContext.Users.FirstOrDefault(u => u.Email == user.Email);
+            if (userEmail == null)
+            {
+                return StatusCode(StatusCodes.Status404NotFound);
+            }
+            var hashedPassword = userEmail.Password;
+            if (!SecurePasswordHasherHelper.Verify(user.Password, hashedPassword))
+                return Unauthorized();
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userEmail.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(ClaimTypes.Name, user.Email),
+                new Claim(ClaimTypes.Role, userEmail.Role),
+           };
+            var token = _auth.GenerateAccessToken(claims);
+            return new ObjectResult(new
+            {
+                access_token = token.AccessToken,
+                token_type = token.TokenType,
+                user_Id = userEmail.Id,
+                user_name = userEmail.Name,
+                expires_in = token.ExpiresIn,
+                creation_Time = token.ValidFrom,
+                expiration_Time = token.ValidTo,
+            });
+        }
+
     }
+
 }
